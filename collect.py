c8f93cc1e29769b56e9fdf776e0d03bab3010ccd
briones <briones@ualberta.ca>
2022-04-09 12:53:16 -0600

    fix: collect command running smoothly

--- collect.py
+++ collect.py
@@ -1,16 +1,62 @@
 import numpy as np
 import cv2
+from constants import IMAGES_FOLDER
+import uuid
+import os
 
-def collect():
+def configFrameTitle(label, current, limit):
+  return 'Collecting ' + label + ' (' + str(current) + ' of ' + str(limit) + "). Press 'c' to capture."
+
+def setupFolders(labels):
+  if not os.path.exists(IMAGES_FOLDER):
+    os.mkdir(IMAGES_FOLDER)
+  
+  for label in labels:
+    path = os.path.join(IMAGES_FOLDER, label)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def collect(limit, labels):
+
+  setupFolders(labels)
+
+  limit = int(limit)
   cap = cv2.VideoCapture(0)
   
+  current = 1
+  labelIndex = 0
+  title = configFrameTitle(labels[labelIndex], current, limit)
+
   while True:
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    cv2.imshow(title, frame)
 
     keypressed = cv2.waitKey(1)
     if keypressed == ord('c'):
-      print('capture!')
+
+      # prevent labelIndex exceeding labels array
+      if labelIndex == len(labels):
+        continue
+
+      # capture current image
+      imgname = os.path.join(IMAGES_FOLDER, labels[labelIndex], labels[labelIndex] + '.' + '{}.jpg'.format(str(uuid.uuid1())))
+      cv2.imwrite(imgname, frame)
+
+      # queue next image
+      current += 1
+
+      # if next image is over the limit do a reset and go the next label
+      if current > limit:
+        current = 1 # reset
+        labelIndex += 1 # next label
+      
+      # if all labels are discovered
+      if labelIndex == len(labels):
+        title = "All labels are captured. Please press 'q' to quit"
+      
+      else:
+        title = configFrameTitle(labels[labelIndex], current, limit)
+
     elif keypressed == ord('q'):
       break
 
--- shell.py
+++ shell.py
@@ -3,15 +3,27 @@ import os
 from collect import collect
 import cv2
 
-IMAGES_FOLDER = "collectedimages"
+class Shell(Cmd):
+  prompt = '>>> '
 
-def shell():
-  while True:
-    command = input(">>> ")
-    if command == "exit":
-      break
+  def do_exit(self, inp):
+    print('Goodbye!')
+    return True
 
-    elif command == "collect":
-      collect()
+  def do_collect(self, inp):
+    parsed = inp.split()
 
-shell()
+    if len(parsed) < 2:
+      print('Need to provide limit and labels (ex: collect 5 thumbsup thumbsdown)')
+    
+    elif parsed[0].isnumeric() == False:
+      print('First argument must be a number followed by labels (ex: collect 5 thumbsup thumbsdown)')
+
+    else:
+      collect(parsed.pop(0), parsed)
+
+  def help_collect(self):
+    print("Use 'collect' command to collect images for labelling")
+    print('Provide a limit followed by labels (ex: collect 5 thumbsup thumbsdown)')
+
+Shell().cmdloop()
